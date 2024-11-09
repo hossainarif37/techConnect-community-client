e45730c904d3ee669fd51fcd478ed39939b8d12a
hossainarif37 <hossainarif4937@gmail.com>
2024-11-09 17:44:47 +0600

    feat: Add image file validation and profile photo preview
    - Implemented file validation to ensure only image files are selected for upload.
    - Added state management to handle the selected image file and display a preview.
    - Updated profile photo component to conditionally render either the selected image or a default placeholder.

--- src/app/(root)/profile/components/ProfileHeader.tsx
+++ src/app/(root)/profile/components/ProfileHeader.tsx
@@ -9,18 +9,30 @@ import { FaCloudUploadAlt } from "react-icons/fa";
 import { useGetUserProfileByIdQuery } from "@/redux/api/endpoints/users/users";
 import { useParams } from "next/navigation";
 import LoadingRound from "@/components/common/LoadingRound";
+import { useState } from "react";
 
 
 const ProfileHeader = () => {
     const params = useParams();
     const { isError, error, isLoading, data } = useGetUserProfileByIdQuery(params?.id);
-
+    const [selectedFile, setSelectedFile] = useState<File | null>(null);
     if (isLoading) {
         return <LoadingRound className="text-2xl text-blue-primary py-20"/>
     }
-
-
     const { name, profilePicture, followers, following } = data?.user;
+
+    const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const file = e.target.files?.[0];
+        if (!file?.type.includes('image')) {
+            alert('Please select an image file!');
+            return;
+        }
+        console.log('file', file);
+        if (file) {
+            setSelectedFile(file);
+        }
+    };
+
 
     const handleUploadPhoto = (e: React.ChangeEvent<HTMLInputElement>) => {
         console.log(e.target.value);
@@ -32,10 +44,13 @@ const ProfileHeader = () => {
             <div className="md:w-[600px] xl:w-[750px] text-white mx-auto flex flex-col lg:flex-row items-center">
 
                 {/* Profile Photo */}
-                <div className="w-28 mb-3 lg:mb-0 lg:w-32 xl:w-48 md:mr-5">
+                <div className="w-28 h-28 overflow-hidden mb-3 rounded-full lg:mb-0 lg:w-32 lg:h-32 xl:w-48 xl:h-48 md:mr-5">
                     <Image
-                        className="w-full"
-                        src={profile_blank_image} alt=""
+                        className={`w-full rounded-full object-cover scale-100 ${selectedFile && 'scale-125'}`}
+                        src={selectedFile ? URL.createObjectURL(selectedFile) : profile_blank_image}
+                        width={300}
+                        height={300}
+                        alt=""
                     />
                 </div>
 
@@ -54,12 +69,11 @@ const ProfileHeader = () => {
 
                     {/* Change Photo Button */}
                     <label htmlFor="upload-photo" className="btn bg-blue-primary cursor-pointer font-semibold border flex items-center border-accent gap-3">
-                        <input onChange={handleUploadPhoto} type="file" className="hidden" id="upload-photo" />
+                        <input onChange={handleFileChange}  type="file" className="hidden" id="upload-photo" accept="image/*"/>
                         <span className="text-2xl"><FaCloudUploadAlt /></span>
                         <span>Upload Photo</span>
                     </label>
                 </div>
-
             </div>
         </section>
     );
