2c49eedbe6d3a0d6491617be0e1fee0b37953a7f
hossainarif37 <hossainarif4937@gmail.com>
2024-11-09 19:49:44 +0600

    refactor: Refactor Profile Header to Support Image Upload with Preview and Toast Notifications
    - Added functionality to upload and preview profile image.
    - Replaced file upload button with conditional rendering for uploading, canceling, or choosing a photo.
    - Introduced `PrimaryButton` component for reusability in upload and cancel actions.
    - Integrated toast notifications on successful image upload.
    - Improved UI/UX for image selection and cancellation.
    - Minor styling adjustments for the upload and cancel buttons.

--- src/app/(root)/profile/components/ProfileHeader.tsx
+++ src/app/(root)/profile/components/ProfileHeader.tsx
@@ -1,25 +1,29 @@
 "use client"
 
-import { IRootState } from "@/types/types";
 import Image from "next/image";
-import { useSelector } from "react-redux";
 import profile_blank_image from "../../../../../public/icons/profile_blank_image.png";
-import Loading from "@/components/common/Loading";
-import { FaCloudUploadAlt } from "react-icons/fa";
 import { useGetUserProfileByIdQuery } from "@/redux/api/endpoints/users/users";
 import { useParams } from "next/navigation";
 import LoadingRound from "@/components/common/LoadingRound";
 import { useState } from "react";
+import { TbPhotoPlus } from "react-icons/tb";
+import PrimaryButton from "@/components/common/Button/PrimaryButton";
+import { FiUpload } from "react-icons/fi";
+import { IoClose } from "react-icons/io5";
+import toast from "react-hot-toast";
 
 
 const ProfileHeader = () => {
     const params = useParams();
     const { isError, error, isLoading, data } = useGetUserProfileByIdQuery(params?.id);
     const [selectedFile, setSelectedFile] = useState<File | null>(null);
+    const [isUploading, setIsUploading] = useState(false);
+    const [imageUrl, setImageUrl] = useState('');
     if (isLoading) {
-        return <LoadingRound className="text-2xl text-blue-primary py-20"/>
+        return <LoadingRound className="text-2xl text-blue-primary py-20" />
     }
     const { name, profilePicture, followers, following } = data?.user;
+
 
     const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const file = e.target.files?.[0];
@@ -27,15 +31,27 @@ const ProfileHeader = () => {
             alert('Please select an image file!');
             return;
         }
-        console.log('file', file);
         if (file) {
             setSelectedFile(file);
         }
     };
 
 
-    const handleUploadPhoto = (e: React.ChangeEvent<HTMLInputElement>) => {
-        console.log(e.target.value);
+    const handleUploadPhoto = () => {
+        console.log('file', selectedFile);
+        setIsUploading(true);
+        new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve('Success');
+                toast.success('Image uploaded successfully.');
+                setImageUrl(URL.createObjectURL(selectedFile!));
+                setSelectedFile(null);
+            }, 2000);
+        }).catch((err) => {
+            console.log('err', err);
+        }).finally(() => {
+            setIsUploading(false);
+        });
     }
 
     return (
@@ -46,8 +62,8 @@ const ProfileHeader = () => {
                 {/* Profile Photo */}
                 <div className="w-28 h-28 overflow-hidden mb-3 rounded-full lg:mb-0 lg:w-32 lg:h-32 xl:w-48 xl:h-48 md:mr-5">
                     <Image
-                        className={`w-full rounded-full object-cover scale-100 ${selectedFile && 'scale-125'}`}
-                        src={selectedFile ? URL.createObjectURL(selectedFile) : profile_blank_image}
+                        className={`w-full rounded-full object-cover scale-100 ${selectedFile || imageUrl && 'scale-125'}`}
+                        src={selectedFile ? URL.createObjectURL(selectedFile) : imageUrl || profile_blank_image}
                         width={300}
                         height={300}
                         alt=""
@@ -68,11 +84,31 @@ const ProfileHeader = () => {
                     </div>
 
                     {/* Change Photo Button */}
-                    <label htmlFor="upload-photo" className="btn bg-blue-primary cursor-pointer font-semibold border flex items-center border-accent gap-3">
-                        <input onChange={handleFileChange}  type="file" className="hidden" id="upload-photo" accept="image/*"/>
-                        <span className="text-2xl"><FaCloudUploadAlt /></span>
-                        <span>Upload Photo</span>
-                    </label>
+                    {
+                        selectedFile ? (
+                            <div className="flex items-center flex-col gap-3">
+                                <PrimaryButton
+                                    onClick={handleUploadPhoto}
+                                    className="flex w-56 justify-center items-center gap-x-1 border text-white bg-blue-primary border-blue-primary font-semibold">
+                                    <span className="text-2xl"><FiUpload /></span>
+                                    <span>{isUploading ? 'Uploading...' : 'Upload Photo'}</span>
+                                </PrimaryButton>
+                                <PrimaryButton
+                                    onClick={() => setSelectedFile(null)}
+                                    className="flex w-56 justify-center items-center gap-x-1 border border-highlight font-semibold">
+                                    <span className="text-2xl"><IoClose /></span>
+                                    <span>Cancel</span>
+                                </PrimaryButton>
+                            </div>
+                        ) :
+                            <label htmlFor="upload-photo" className="w-56 py-3 rounded-lg gap-x-1 bg-text-white border  border-highlight cursor-pointer font-semibold">
+                                <input onChange={handleFileChange} type="file" className="hidden" id="upload-photo" accept="image/*" />
+                                <div className="flex justify-center items-center gap-x-2">
+                                    <span className="text-2xl"><TbPhotoPlus /></span>
+                                    <span>Choose Photo</span>
+                                </div>
+                            </label>
+                    }
                 </div>
             </div>
         </section>
