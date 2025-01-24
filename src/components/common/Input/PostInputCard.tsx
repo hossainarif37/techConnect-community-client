dd9730faca237ebbf4505f300ecbb24c881fb8ad
hossainarif37 <hossainarif4937@gmail.com>
2025-01-25 00:23:18 +0600

    refactor: Improve modal handling in PostInputCard component for better readability

--- src/components/common/Input/PostInputCard.tsx
+++ src/components/common/Input/PostInputCard.tsx
@@ -9,13 +9,16 @@ import { IRootState } from "@/types/types";
 
 const PostInputCard = () => {
     const [isModalOpen, setIsModalOpen] = useState(false);
-    const {user} = useSelector((state: IRootState) => state.userSlice);
+    const { user } = useSelector((state: IRootState) => state.userSlice);
+    const handleModalOpen = () => {
+        setIsModalOpen(true)
+    }
 
     return (
         <div className="bg-secondary rounded-xl flex p-3 lg:px-10 lg:py-5 xl:py-10 xl:px-10 gap-5 w-full mx-auto lg:w-[500px] xl:w-[650px]">
             <UserImage profilePicture={user?.profilePicture} className="w-12 lg:w-14 xl:w-16 h-12 lg:h-14 xl:h-16" />
             <button
-                onClick={()=>setIsModalOpen(true)}
+                onClick={handleModalOpen}
                 className="flex-1 rounded-full text-center md:text-left text-nowrap text-base md:text-lg font-semibold md:pl-10 bg-accent text-white  hover:bg-highlight duration-100" type="button">
                 Share Your Experience
             </button>
